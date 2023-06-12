b288a47589ae195fe8f5451a9631075ab4bf13d3
konstantin.korobko@outlook.com <konstantin.korobko@outlook.com>
2023-06-12 17:10:01 +0300

    dev server settings

---
+++ GamesShopApp/GamesShopApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Google.Protobuf.WellKnownTypes;
+
+namespace GamesShopApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? userName { get; set; }
+        public string? passwordHash { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public Timestamp? createdAt { get; set; }
+        public Timestamp? modifiedAt { get; set; }
+    }
+}
